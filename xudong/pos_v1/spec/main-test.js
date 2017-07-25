5ff90ccd0c40906bcb28cc0de6268c21d3cedbc8
diaojinping <1466797443@qq.com>
2017-07-25 10:39:48 +0800

    buildItem

--- xudong/pos_v1/spec/main-test.js
+++ xudong/pos_v1/spec/main-test.js
@@ -1,41 +1,12 @@
-'use strict'
-
-describe('entire', () => {
-
-    let inputs = [
-        'ITEM000000',
-        'ITEM000000',
-        'ITEM000000',
-        'ITEM000001',
-        'ITEM000001',
-        'ITEM000004-4'
-    ];
-    let testData = `
-***<没钱赚商店>收据***
-名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：6.00(元)
-名称：雪碧，数量：2瓶，单价：3.00(元)，小计：6.00(元)
-名称：电池，数量：4个，单价：2.00(元)，小计：6.00(元)
-----------------------
-总计：18.00(元)
-节省：5.00(元)
-**********************`.trim();
-
-
-    it('entire-test', () => {
-        spyOn(console, 'log');
-        printReceipt(inputs);
-        expect(console.log).toHaveBeenCalledWith(testData);
-    });
-});
-describe('unit', () => {
-    describe('buildItem-test', () => {
-        let inputs = [
+'yse strict'
+describe('unit-test', () => {
+    describe('buildItem', () => {
+        let inputs = ['ITEM000000',
             'ITEM000000',
             'ITEM000000',
-            'ITEM000000',
-            'ITEM000001',
-            'ITEM000001',
-            'ITEM000004-4'
+            'ITEM000002',
+            'ITEM000002',
+            'ITEM000003-4',
         ];
         let testData = [
             {
@@ -45,270 +16,30 @@ describe('unit', () => {
                     unit: '瓶',
                     price: 3.00
                 },
-                count: 3
+                count: 3,
             },
             {
                 item: {
-                    barcode: 'ITEM000001',
-                    name: '雪碧',
-                    unit: '瓶',
-                    price: 3.00
+                    barcode: 'ITEM000002',
+                    name: '苹果',
+                    unit: '斤',
+                    price: 5.50
                 },
                 count: 2
             },
             {
                 item: {
-                    barcode: 'ITEM000004',
-                    name: '电池',
-                    unit: '个',
-                    price: 2.00
+                    barcode: 'ITEM000003',
+                    name: '荔枝',
+                    unit: '斤',
+                    price: 15.00
                 },
                 count: 4
             }
-        ];
-        it('test1', () => {
+
+        ]
+        it('test-1', () => {
             expect(buildItem(inputs)).toEqual(testData);
         });
     });
-    describe('buildItem-test', () => {
-        let inputs = [
-            {
-                item: {
-                    barcode: 'ITEM000000',
-                    name: '可口可乐',
-                    unit: '瓶',
-                    price: 3.00
-                },
-                count: 3
-            },
-            {
-                item: {
-                    barcode: 'ITEM000001',
-                    name: '雪碧',
-                    unit: '瓶',
-                    price: 3.00
-                },
-                count: 2
-            },
-            {
-                item: {
-                    barcode: 'ITEM000004',
-                    name: '电池',
-                    unit: '个',
-                    price: 2.00
-                },
-                count: 4
-            }
-        ];
-        let testData = [
-            {
-                cartItem: {
-                    item: {
-                        barcode: 'ITEM000000',
-                        name: '可口可乐',
-                        unit: '瓶',
-                        price: 3.00
-                    },
-                    count: 3
-                },
-                subtotal: 6.00,
-                save: 3.00,
-            },
-            {
-                cartItem: {
-                    item: {
-                        barcode: 'ITEM000001',
-                        name: '雪碧',
-                        unit: '瓶',
-                        price: 3.00
-                    },
-                    count: 2
-                },
-                subtotal: 6.00,
-                save: 0
-            },
-            {
-                cartItem: {
-                    item: {
-                        barcode: 'ITEM000004',
-                        name: '电池',
-                        unit: '个',
-                        price: 2.00
-                    },
-                    count: 4
-                },
-                subtotal: 6.00,
-                save: 2.00,
-            }
-        ];
-        it('test_2', () => {
-            expect(getSubtotal(inputs)).toEqual(testData);
-        });
-    });
-    describe('getTotal', () => {
-        let inputs = [
-            {
-                cartItem: {
-                    item: {
-                        barcode: 'ITEM000000',
-                        name: '可口可乐',
-                        unit: '瓶',
-                        price: 3.00
-                    },
-                    count: 3
-                },
-                subtotal: 6.00,
-                save: 3.00,
-            },
-            {
-                cartItem: {
-                    item: {
-                        barcode: 'ITEM000001',
-                        name: '雪碧',
-                        unit: '瓶',
-                        price: 3.00
-                    },
-                    count: 2
-                },
-                subtotal: 6.00,
-                save: 0
-            },
-            {
-                cartItem: {
-                    item: {
-                        barcode: 'ITEM000004',
-                        name: '电池',
-                        unit: '个',
-                        price: 2.00
-                    },
-                    count: 4
-                },
-                subtotal: 6.00,
-                save: 2.00,
-            }
-        ];
-        let testData = [
-            {
-                receiptItem: {
-                    cartItem: {
-                        item: {
-                            barcode: 'ITEM000000',
-                            name: '可口可乐',
-                            unit: '瓶',
-                            price: 3.00
-                        },
-                        count: 3
-                    },
-                    subtotal: 6.00,
-                    save: 3.00,
-                },
-                allsave: 5.00,
-                total: 18.00,
-            },
-            {
-                receiptItem: {
-                    cartItem: {
-                        item: {
-                            barcode: 'ITEM000001',
-                            name: '雪碧',
-                            unit: '瓶',
-                            price: 3.00
-                        },
-                        count: 2
-                    },
-                    subtotal: 6.00,
-                    save: 0
-                },
-                allsave: 5.00,
-                total: 18.00,
-            },
-            {
-                receiptItem: {
-                    cartItem: {
-                        item: {
-                            barcode: 'ITEM000004',
-                            name: '电池',
-                            unit: '个',
-                            price: 2.00
-                        },
-                        count: 4
-                    },
-                    subtotal: 6.00,
-                    save: 2.00,
-                },
-                allsave: 5.00,
-                total: 18.00,
-            }
-        ];
-        it('test_3', () => {
-            expect(getTotal(inputs)).toEqual(testData);
-        });
-    });
-    describe('getTotal', () => {
-        let inputs = [
-            {
-                receiptItem: {
-                    cartItem: {
-                        item: {
-                            barcode: 'ITEM000000',
-                            name: '可口可乐',
-                            unit: '瓶',
-                            price: 3.00
-                        },
-                        count: 3
-                    },
-                    subtotal: 6.00,
-                    save: 3.00,
-                },
-                allsave: 5.00,
-                total: 18.00,
-            },
-            {
-                receiptItem: {
-                    cartItem: {
-                        item: {
-                            barcode: 'ITEM000001',
-                            name: '雪碧',
-                            unit: '瓶',
-                            price: 3.00
-                        },
-                        count: 2
-                    },
-                    subtotal: 6.00,
-                    save: 0
-                },
-                allsave: 5.00,
-                total: 18.00,
-            },
-            {
-                receiptItem: {
-                    cartItem: {
-                        item: {
-                            barcode: 'ITEM000004',
-                            name: '电池',
-                            unit: '个',
-                            price: 2.00
-                        },
-                        count: 4
-                    },
-                    subtotal: 6.00,
-                    save: 2.00,
-                },
-                allsave: 5.00,
-                total: 18.00,
-            }
-        ];
-        let testData = `
-***<没钱赚商店>收据***
-名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：6.00(元)
-名称：雪碧，数量：2瓶，单价：3.00(元)，小计：6.00(元)
-名称：电池，数量：4个，单价：2.00(元)，小计：6.00(元)
-----------------------
-总计：18.00(元)
-节省：5.00(元)
-**********************`.trim();
-        it('test_4', () => {
-            expect(getString(inputs)).toEqual(testData);
-        })
-    });
-});
+})
--- xudong/pos_v1/src/main.js
+++ xudong/pos_v1/src/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-let printReceipt=(inputs)=>{
-    let cartItems=buildItem(inputs);
-    let receiptItems=getSubtotal(cartItems);
-    let receipt=getTotal(receiptItems);
-    let receiptText=getString(receipt);
-    console.log(receiptText);
-}
-
 let buildItem = (inputs) => {
     let allItems = loadAllItems();
     let cartItems = [];
@@ -24,40 +16,4 @@ let buildItem = (inputs) => {
         }
     }
     return cartItems;
-}
-let getSubtotal = (cartItems) => {
-    let subtotalItems = cartItems.map((cartItem) => {
-        let saveCount = parseInt(cartItem.count / 3);
-        let save = saveCount * cartItem.item.price;
-        let subtotal = (cartItem.count - saveCount) * cartItem.item.price;
-        return {cartItem, save, subtotal};
-    });
-    return subtotalItems;
-}
-
-let getTotal = (receiptItems) => {
-    let total = 0;
-    let allsave = 0;
-    for (let receiptItem of receiptItems) {
-        total += receiptItem.subtotal;
-        allsave += receiptItem.save;
-    }
-    let receipt = receiptItems.map((receiptItem) => {
-        return {receiptItem, total, allsave};
-    });
-    return receipt;
-}
-let getString = (receipt) => {
-    let receiptString = "***<没钱赚商店>收据***";
-    for (let itemArray of receipt) {
-        receiptString += '\n名称：' + itemArray.receiptItem.cartItem.item.name + '，数量：'
-            + itemArray.receiptItem.cartItem.count + itemArray.receiptItem.cartItem.item.unit + '，单价：'
-            + itemArray.receiptItem.cartItem.item.price.toFixed(2) + '(元)，小计：'
-            + itemArray.receiptItem.subtotal.toFixed(2) + '(元)';
-    }
-    receiptString += '\n----------------------' +
-        '\n总计：' + receipt[0].total.toFixed(2) + '(元)' +
-        '\n节省：' + receipt[0].allsave.toFixed(2) + '(元)' +
-        '\n**********************';
-    return receiptString;
 }
